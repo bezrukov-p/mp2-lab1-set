f83878a82165536338fe574bd994970849c016fd
unknown <Hegativenn@yandex.ru>
2018-09-15 22:29:25 +0300

    Закомментил

--- src/tbitfield.cpp
+++ src/tbitfield.cpp
@@ -19,68 +19,68 @@ TBitField::~TBitField()
 {
 }
 
-int TBitField::GetMemIndex(const int n) const // индекс Мем для бита n
-{
-}
-
-TELEM TBitField::GetMemMask(const int n) const // битовая маска для бита n
-{
-}
-
-// доступ к битам битового поля
-
-int TBitField::GetLength(void) const // получить длину (к-во битов)
-{
-  return 0;
-}
-
-void TBitField::SetBit(const int n) // установить бит
-{
-}
-
-void TBitField::ClrBit(const int n) // очистить бит
-{
-}
-
-int TBitField::GetBit(const int n) const // получить значение бита
-{
-  return 0;
-}
-
-// битовые операции
-
-TBitField& TBitField::operator=(const TBitField &bf) // присваивание
-{
-}
-
-int TBitField::operator==(const TBitField &bf) const // сравнение
-{
-  return 0;
-}
-
-int TBitField::operator!=(const TBitField &bf) const // сравнение
-{
-  return 0;
-}
-
-TBitField TBitField::operator|(const TBitField &bf) // операция "или"
-{
-}
-
-TBitField TBitField::operator&(const TBitField &bf) // операция "и"
-{
-}
-
-TBitField TBitField::operator~(void) // отрицание
-{
-}
-
-// ввод/вывод
-
-istream &operator>>(istream &istr, TBitField &bf) // ввод
-{
-}
-
-ostream &operator<<(ostream &ostr, const TBitField &bf) // вывод
-{
-}
+//int TBitField::GetMemIndex(const int n) const // индекс Мем для бита n
+//{
+//}
+//
+//TELEM TBitField::GetMemMask(const int n) const // битовая маска для бита n
+//{
+//}
+//
+//// доступ к битам битового поля
+//
+//int TBitField::GetLength(void) const // получить длину (к-во битов)
+//{
+//  return 0;
+//}
+//
+//void TBitField::SetBit(const int n) // установить бит
+//{
+//}
+//
+//void TBitField::ClrBit(const int n) // очистить бит
+//{
+//}
+//
+//int TBitField::GetBit(const int n) const // получить значение бита
+//{
+//  return 0;
+//}
+//
+//// битовые операции
+//
+//TBitField& TBitField::operator=(const TBitField &bf) // присваивание
+//{
+//}
+//
+//int TBitField::operator==(const TBitField &bf) const // сравнение
+//{
+//  return 0;
+//}
+//
+//int TBitField::operator!=(const TBitField &bf) const // сравнение
+//{
+//  return 0;
+//}
+//
+//TBitField TBitField::operator|(const TBitField &bf) // операция "или"
+//{
+//}
+//
+//TBitField TBitField::operator&(const TBitField &bf) // операция "и"
+//{
+//}
+//
+//TBitField TBitField::operator~(void) // отрицание
+//{
+//}
+//
+//// ввод/вывод
+//
+//istream &operator>>(istream &istr, TBitField &bf) // ввод
+//{
+//}
+//
+//ostream &operator<<(ostream &ostr, const TBitField &bf) // вывод
+//{
+//}
